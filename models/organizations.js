4aa00d1cefbc01562378914f33371282fd166cf6
Victor Lourng <hello@victorlourng.com>
2014-05-15 16:42:56 +0000

    Framework and session view

---
+++ models/organizations.js
@@ -0,0 +1,38 @@
+var keystone = require('keystone'),
+	Types = keystone.Field.Types;
+
+/**
+ * Organizations Model
+ * ===================
+ */
+
+var Organization = new keystone.List('Organization', {
+	autokey: { path: 'key', from: 'name', unique: true }
+});
+
+Organization.add({
+	name: { type: String, index: true },
+	logo: { type: Types.CloudinaryImage },
+	website: Types.Url,
+	isHiring: Boolean,
+	description: { type: Types.Markdown },
+	location: Types.Location
+});
+
+
+/**
+ * Relationships
+ * =============
+ */
+
+Organization.relationship({ ref: 'User', refPath: 'organization', path: 'members' });
+
+
+/**
+ * Registration
+ * ============
+ */
+
+Organization.addPattern('standard meta');
+Organization.defaultColumns = 'name, website, isHiring';
+Organization.register();
