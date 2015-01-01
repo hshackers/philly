08a5cc95644227663ebca6b4df567d8bdb61b079
Victor Lourng <hello@victorlourng.com>
2014-12-31 23:53:28 -0500

    Remove blog link

--- routes/middleware.js
+++ routes/middleware.js
@@ -16,7 +16,7 @@ exports.initLocals = function(req, res, next) {
 		{ label: 'About',		key: 'about',		href: '/about' },
 		{ label: 'Meetups',		key: 'meetups',		href: '/meetups' },
 		{ label: 'Members',		key: 'members',		href: '/members' },
-		{ label: 'Blog',		key: 'blog',		href: '/blog' },
+		{ label: 'Links',		key: 'links',		href: '/links' },
 		{ label: 'Showbag',		key: 'showbag',		href: '/showbag' }
 	];
 	
